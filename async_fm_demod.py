85b9d43dda8404f2a13be60e04e1edb6618a09a9
Yuval Adam <_@yuv.al>
2018-04-12 13:13:18 +0300

    Cleanup

--- async_fm_demod.py
+++ async_fm_demod.py
@@ -1,24 +1,14 @@
 import asyncio
 from rtlsdr import RtlSdr
 
-RATE_IN = RATE_OUT = 170000
-RATE_OUT2 = 32000
-CUSTOM_ATAN = 1
-DEEMPH = 1
-SQUELCH = 0
-
 async def streaming():
     sdr = RtlSdr()
 
     async for samples in sdr.stream():
-        # do something with samples
-        # ...
-        print(len(samples), samples[0])
+        print(len(samples), samples[0], type(samples[0]))
 
-    # to stop streaming:
     await sdr.stop()
 
-    # done
     sdr.close()
 
 loop = asyncio.get_event_loop()
--- asynciotest.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-async def echo():
-    pass
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_forever(echo())
