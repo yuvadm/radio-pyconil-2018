f75a67be8250934a408361d95311fb571b37924d
Yuval Adam <_@yuv.al>
2018-04-12 14:00:30 +0300

    Better sample rates

--- async_fm_demod.py
+++ async_fm_demod.py
@@ -7,7 +7,7 @@ from rtlsdr import RtlSdr
 
 async def streaming():
     sdr = RtlSdr()
-    sdr.sample_rate = 1800000
+    sdr.sample_rate = 1.2e6
     sdr.center_freq = 91.8e6
 
     audio = None
@@ -15,9 +15,9 @@ async def streaming():
 
     async for samples in sdr.stream():
         samples = np.array(samples).astype('complex64')
-        samples = signal.decimate(samples, int(1800000 / 200000))
+        samples = signal.decimate(samples, int(1.2e6 / 200e3))
         samples = np.angle(samples[1:] * np.conj(samples[:-1]))
-        samples = signal.decimate(samples, int(200000 / 44100))
+        samples = signal.decimate(samples, int(200e3 / 50e3))
         samples *= 10000
 
         if audio is not None:
@@ -26,7 +26,7 @@ async def streaming():
             audio = samples.astype('int16')
 
         i += 1
-        if i > 200:
+        if i > 100:
             break
         else:
             print(len(audio))
