02b3687f41d8f0c265c2d641e40973a1b203f701
Yuval Adam <_@yuv.al>
2018-04-12 14:04:12 +0300

    Add de-emph filter

--- async_fm_demod.py
+++ async_fm_demod.py
@@ -17,6 +17,10 @@ async def streaming():
         samples = np.array(samples).astype('complex64')
         samples = signal.decimate(samples, int(1.2e6 / 200e3))
         samples = np.angle(samples[1:] * np.conj(samples[:-1]))
+
+        x = np.exp(-1 / (200e3 * 75e-6))
+        samples = signal.lfilter([1-x], [1,-x], samples)
+
         samples = signal.decimate(samples, int(200e3 / 50e3))
         samples *= 10000
 
